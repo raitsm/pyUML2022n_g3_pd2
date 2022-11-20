7fcf091cd8fc13ae329fc84df411218a3bcedc0a
Raits Misins <raits.misins@yahoo.com>
2022-11-20 20:00:44 +0200

    Nitty-gritty fine tuning.

--- pd2_RM.py
+++ pd2_RM.py
@@ -1,13 +1,14 @@
 import turtle
 import math
 
-POINTS = 11         
+POINTS = 8         
 FULL_CIRCLE = 360       
 # jo lielāka starpība starp RADIUS_DIFFERENCE un CENTER_RADIUS_BASE, jo izteiktāki stari zvaigznei
 # ja abi būs vienādi, tad, 3-staru zvaigzne izskatīsies līdzīga vienādmalu trijstūrim
 CENTER_RADIUS_BASE = 30     # zvaigznes iekšējais rādiuss
 RADIUS_DIFFERENCE = 60      # starpība starp zvaigznes iekšējo un ārējo rādiusu (faktiski stara garums)
-RAY_LENGTH = CENTER_RADIUS_BASE + RADIUS_DIFFERENCE     # garums no stara virsotnes līdz bāzes riņķa līnijas centram
+RAY_LENGTH_BASE = CENTER_RADIUS_BASE + RADIUS_DIFFERENCE     # garums no stara virsotnes līdz bāzes riņķa līnijas centram
+MARGIN = 40
 
 # risinājums, kurā zvaigzne tiek zīmēta no leņķiem, kas tiek secīgi grozīti
 # 3- un 4-staru gadījumā vai nu paliek nelielas atstarpes starp staru pamatnēm,
@@ -18,7 +19,7 @@ def use_angles(num_points: int=POINTS):
     increment_angle = 360 / num_points
     star_angle = increment_angle/5
     for i in range(num_points):
-        draw_angle(increment_angle * i, star_angle, RAY_LENGTH)
+        draw_angle(increment_angle * i, star_angle, RAY_LENGTH_BASE)
 
 def draw_angle(direction: int, star_angle: int, height: int):
 # draw_angle zīmē leņķi norādītajā virzienā (virzienu norāda grādos)
@@ -55,9 +56,10 @@ def draw_angle(direction: int, star_angle: int, height: int):
 def use_lines(num_points: int=POINTS):
     
     calculated_radius = CENTER_RADIUS_BASE + num_points/2
-    calculated_ray_length = RAY_LENGTH + num_points
+    calculated_ray_length = RAY_LENGTH_BASE + num_points
     turtle.penup()
-    turtle.speed("fastest")                  
+    turtle.speed("fastest")
+    turtle.color("red")                  
     for i in range(num_points):
         # formulas punktu izvietošanai uz riņķa līnijas aizgūtas no stackoverflow.com
         angle_center = 2 * math.pi * i / num_points
@@ -87,9 +89,18 @@ def num_points_wrapper() -> int:
             pass
     return
 
+# ekrāna konfigurācija: izmērs un fona krāsa
+def setup_screen(size: float,bg_color: str="white"):
+    turtle.setup(width=size, height=size)
+    turtle.screensize(canvwidth=size*2, canvheight=size*2,bg=bg_color) #  
+    return
+
+
 def main():
     # num_points = POINTS
     num_points = num_points_wrapper()
+
+    setup_screen((RAY_LENGTH_BASE + num_points) + MARGIN*2, bg_color="grey")
 
     use_lines(num_points)
     # use_angles(num_points)
